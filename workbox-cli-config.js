6a6dfbc86410b510efd34861958db2c58173ad0c
Kevin Logan <logk34@hotmail.com>
2017-06-19 16:19:24 -0500

    sw file that was missed

---
+++ workbox-cli-config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  "globDirectory": "src\\",
+  "globPatterns": [
+    "**/*.{html,ts,zip,png,xml,ico,svg,json}"
+  ],
+  "swDest": "build/sw.js",
+  "globIgnores": [
+    "..\\workbox-cli-config.js"
+  ]
+};
