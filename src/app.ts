33bba1e6105cb34be499d9551f9b464be5fa7778
Kevin Logan <logk34@hotmail.com>
2017-06-19 16:18:02 -0500

    host as https

--- src/app.ts
+++ src/app.ts
@@ -1,3 +1,24 @@
 export class App {
   message = 'Hello World!';
+  /**
+   *
+   */
+  constructor() {
+    this.registerServiceWorker();
+  }
+
+  private registerServiceWorker(){
+debugger;
+    if ('serviceWorker' in navigator) {
+    window.addEventListener('load', function() {
+      navigator.serviceWorker.register('/src/build/sw.js').then(function(registration) {
+        // Registration was successful
+        console.log('ServiceWorker registration successful with scope: ', registration.scope);
+      }, function(err) {
+        // registration failed :(
+        console.log('ServiceWorker registration failed: ', err);
+      });
+    });
+  }
+  }
 }
