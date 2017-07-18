97936e82ef380d5dbef56f9b8790d6096be4ebb0
Kevin Logan <logk34@hotmail.com>
2017-07-18 15:34:32 -0500

    fixed misspelling

--- cache-sw.js
+++ cache-sw.js
@@ -1,36 +1,76 @@
 // from Pluralsight and 
 // event.waitUntil from https://developers.google.com/web/fundamentals/getting-started/primers/service-workers
-var version = 1;
+var version = 2;
 var CACHE_NAME = 'my-site-cache-v1';
 var urlsToCache = [
+    '/',
+    'cache-sw.js',
     '/src/resources/elements/offline.html',
     '/scripts/app-bundle.js',
     '/scripts/vendor-bundle.js',
-    '/src/manifest.json',
+    '/src/manifest.json'
 ];
 self.addEventListener('install', function (event) {
-    self.skipWaiting();
+    console.log('WORKER: Installing');
+    caches.delete(CACHE_NAME);
     event.waitUntil(
-        caches.open(version)
-            .then(function (cache) {
-                return cache.addAll(urlsToCache);
-            })
+        caches.open(CACHE_NAME)
+            .then(cache => { cache.addAll(urlsToCache); })
+            .then(_ => self.skipWaiting())
     );
 });
 
-self.addEventListener('fetch', function (event) {
-    event.respondWith(
-        caches.match(event.request)
-            .then(function (res) {
-                if (res) {
-                    return res;
-                }
+self.addEventListener('activate', function (event) {
+    // cleanup
+    // event.waitUntil(
+    //     caches.keys()
+    //         .then(function (keys) {
+    //             return Promise.all(keys.filter(function (key) {
+    //                 return key !== version;
+    //             }).map(function (key) {
+    //                 return caches.delete(key);
+    //             }));
+    //         })
+    // );
+});
 
-                if (!navigator.onLine) {
-                    return caches.match(new Request('/src/resources/elements/offline.html'));
-                }
-console.log(event.request);
-                return fetch(event.request);
+self.addEventListener('fetch', function (event) {
+
+    if (event.request.method !== 'GET') {
+        return;
+    }
+
+    // event.respondWith(
+    //     caches.match(event.request)
+    //         .then(function (res) {
+    //             if (res) {
+    //                 return res;
+    //             }
+
+    //             if (!navigator.onLine) {
+    //                 return caches.match(new Request('/src/resources/elements/offline.html'));
+    //             }
+
+    //             console.log(event.request);
+    //             return fetch(event.request);
+    //         })
+    // );
+
+    // https://www.youtube.com/watch?v=MiLAE6HMr10 - Steve Sanderson at NDC 2017 15 minutes
+    // serve cache if slow
+    event.respondWith(
+        fetch(event.request)
+            .then(networkRespose => {
+                // update if fast enough
+                console.log(`Worker: Updating cached data for ${event.request.url}`);
+                var responseClone = networkRespose.clone();
+                caches.open(CACHE_NAME).then(cache => cache.push(event.request));
+                return networkRespose
             })
-    );
+            .timeout(1000)
+            .catch(_ => {
+                console.log(`Worker: service ${event.request.url} from CACHE`);
+                return caches.match(event.request);
+            })
+    )
 });
